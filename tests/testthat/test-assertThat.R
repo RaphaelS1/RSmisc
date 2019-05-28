76930197fb58873e2f2ed0ac9107839c58d88de2
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-05-28 16:25:32 +0100

    More tests

--- tests/testthat/test-assertThat.R
+++ tests/testthat/test-assertThat.R
@@ -5,9 +5,12 @@ context("assertThat")
 test_that("assertThat",{
   x = 1:10
   cond = inherits(x, "integer")
+  failed_cond = inherits(x,"character")
   errormsg = "Not integer"
   expect_silent(assertThat(x, cond, errormsg))
+  expect_error(assertThat(x, failed_cond, errormsg))
   expect_silent(checkThat(cond, errormsg))
+  expect_equal(checkThat(failed_cond, errormsg),"Not integer")
   expect_silent(testThat(cond))
   expect_silent(isThat(cond))
 })
