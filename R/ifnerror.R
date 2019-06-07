d9a1778b29bac89567e85a19efbb7906b300f661
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-06-07 11:01:39 +0100

    Update ifnerror.R

--- R/ifnerror.R
+++ R/ifnerror.R
@@ -3,24 +3,30 @@
 #' otherwise does whatever is given as argument.
 #' @param expr expression to test for error.
 #' @param noerror expression to be called if no error.
-#' @param error one of 'warn' or 'stop', passed to stopwarn.
+#' @param error one of 'warn', 'stop', or an expression. See Details.
 #' @param silent logical, if TRUE (default) then error returned silently.
 #' @seealso \code{\link{stopwarn}}
 #' @details Tests for an error or nerror (no error) from given expression. If an error is found
-#' then either returns silent, NULL with warning or breaks code.
-#' If nerror then returns the given expression.
+#' then either calls stopwarn if 'warn', 'stop' or 'NULL' are supplied, otherwise evaluates
+#' the given expression. If nerror then returns the given expression.
 #'
 #' @examples
 #' \dontrun{
 #' ifnerror(1/2,print("Success"))
 #' ifnerror(1/"a",print("Success"),"stop",silent = F)
+#' ifnerror(1/"a",print("Nerror"),print("Error!"),silent = T)
 #' }
 #'
 #' @export
-ifnerror <- function(expr, noerror, error = "warn", silent = T){
+ifnerror <- function(expr, noerror, error = NULL, silent = T){
   x = try(expr, silent)
   if(inherits(x, "try-error")){
-    if(!silent) stopwarn(error, "Error not Nerror!")
+    if(is.null(error) | error == "warn")
+      stopwarn("warn", "Error not Nerror!")
+    else if(error == "stop")
+      stopwarn("stop", "Error not Nerror!")
+    else
+      error
   } else {
     noerror
   }
